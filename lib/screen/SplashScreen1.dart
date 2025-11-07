ff9601f104c0ffca0cccf176d2d7212221335f0d
cendy <sendyaja17@gmail.com>
2025-11-07 08:16:00 +0700

    membuat scaffold pada Splash Screen 1

---
+++ lib/screen/SplashScreen1.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen1 extends StatelessWidget {
+  const Splashscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            
+          );
+  }
+}
